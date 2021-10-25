c5164b34be0fc8331a2db5e3bba850dd8f2cfee1
alphonce mutuku <scylla@Scylla-Technology.local>
2021-10-25 14:48:00 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CarouselContainer from '../components/CarouselContainer/CarouselContainer';
+import Navbar from '../components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <CarouselContainer />
+        </>
+    )
+}
+
+export default Home;
